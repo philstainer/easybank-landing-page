4cbade9c25590f0bfaa34e6e895f8f4a18ec070b
Phil Stainer <philstainer.dev@gmail.com>
2020-10-17 21:09:11 +0100

    Add finshed files

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
 ;(function () {
-  console.log('object')
+  const btnMenu = document.querySelector('#btnMenu')
+  const header = document.querySelector('#header')
+  const fadeElements = document.querySelectorAll('.has-fade')
+
+  btnMenu.addEventListener('click', function () {
+    header.classList.toggle('open')
+
+    if (header.classList.contains('open')) {
+      document.body.classList.add('noScroll')
+
+      fadeElements.forEach(function (element) {
+        element.classList.remove('fade-out')
+        element.classList.add('fade-in')
+      })
+    } else {
+      document.body.classList.remove('noScroll')
+
+      fadeElements.forEach(function (element) {
+        element.classList.remove('fade-in')
+        element.classList.add('fade-out')
+      })
+    }
+  })
 })()
